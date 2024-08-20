6b3837737b0e6bf2110cd793fcbb59fb5722c363
mrsayilir <sy.lr@yandex.com>
2024-08-20 10:43:53 +0300

    post metod added

--- src/main/java/net/sayilir/api/controller/GreetingController.java
+++ src/main/java/net/sayilir/api/controller/GreetingController.java
@@ -5,10 +5,7 @@ import net.sayilir.api.common.ResponseUtil;
 import net.sayilir.api.model.Greeting;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -23,7 +20,13 @@ public class GreetingController {
     @GetMapping("/greeting")
     public ResponseEntity<ApiResponse<Greeting>> getGreeting(@RequestParam(value="name", defaultValue="World") String name) {
         Greeting greeting=new Greeting(counter.incrementAndGet(),String.format(template,name));
-        ApiResponse<Greeting> response= ResponseUtil.success(greeting,"Greeting created successfully","/api/greeting/greeting");
+        ApiResponse<Greeting> response= ResponseUtil.success(greeting,"Greeting created successfully","/api/greeting");
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+    @PostMapping("/greeting/add")
+    public ResponseEntity<ApiResponse<Greeting>> addGreeting(@RequestBody Greeting greeting) {
+        System.out.println(greeting);
+        ApiResponse<Greeting> response= ResponseUtil.success(greeting,"Greeting added successfully","/api/greeting/add");
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
